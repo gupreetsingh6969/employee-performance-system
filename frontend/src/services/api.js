28c54dae76cccc3475ec9081b2fd2579e49fb7e1
Gagandhiman-sg <gurpreetsinghdhiman26@gmail.com>
2026-05-23 15:52:34 +0530

    Connected frontend with Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://YOUR-BACKEND-URL.com"
+  baseURL: "https://employee-performance-system-production.up.railway.app"
 });
 
 api.interceptors.request.use(
-(config)=>{
-const token=localStorage.getItem("token");
+(config) => {
+
+const token = localStorage.getItem("token");
 
 if(token){
-config.headers.Authorization=`Bearer ${token}`;
+config.headers.Authorization = `Bearer ${token}`;
 }
 
 return config;
+
 },
 (error)=>{
 return Promise.reject(error);
